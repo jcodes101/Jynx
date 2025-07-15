6ad9e994f284b901bab38f48a747e45917b8bdfe
jcodes <thejcodes101@gmail.com>
2025-07-15 13:33:11 -0400

    - fixes for css 1

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,12 +1,43 @@
 class Tree < ApplicationRecord
-    validates :instagram, presence: true, format: { with: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/,
-    message: "Invalid URL format" }
+  extend FriendlyId
+  friendly_id :name, use: :slugged
 
-    validates :name, presence: true, length: { minimum: 5, message: "must be at least 5 characters long" }
+  belongs_to :user
 
-    extend FriendlyId
-    friendly_id :name, use: :slugged
+  attr_accessor :show_instagram, :show_youtube, :show_x, :show_github
 
+  validates :name, presence: true, length: { minimum: 5, message: "must be at least 5 characters long" }
 
-    belongs_to :user
+  # Conditional social link validations
+  validates :instagram, presence: true,
+                        format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: "Invalid URL format" },
+                        if: :instagram_enabled?
+
+  validates :youtube, presence: true,
+                      format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: "Invalid URL format" },
+                      if: :youtube_enabled?
+
+  validates :x, presence: true,
+                format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: "Invalid URL format" },
+                if: :x_enabled?
+
+  validates :github, presence: true,
+                     format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: "Invalid URL format" },
+                     if: :github_enabled?
+
+  def instagram_enabled?
+    ActiveModel::Type::Boolean.new.cast(show_instagram)
+  end
+
+  def youtube_enabled?
+    ActiveModel::Type::Boolean.new.cast(show_youtube)
+  end
+
+  def x_enabled?
+    ActiveModel::Type::Boolean.new.cast(show_x)
+  end
+
+  def github_enabled?
+    ActiveModel::Type::Boolean.new.cast(show_github)
+  end
 end
